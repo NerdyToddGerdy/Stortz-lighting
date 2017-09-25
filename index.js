12aca3527a5338fb401d879e83ab9abd19ca2d11
Todd Gerdy <toddgerdy@Todds-iMac.local>
2017-09-25 13:37:48 -0400

    removed cloudinary

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
-const cloudinary = require('cloudinary')
+// const cloudinary = require('cloudinary')
 
 app.use(express.static('public'));
 
-cloudinary.config({
-   cloud_name: 'toddles',
-   api_key: '464326275517746',
-   api_secret: 'SGFxqvO4lzuxU8GoBl4rVf7gkZc'
-});
+// cloudinary.config({
+//    cloud_name: 'toddles',
+//    api_key: '464326275517746',
+//    api_secret: 'SGFxqvO4lzuxU8GoBl4rVf7gkZc'
+// });
 
 // cloudinary.uploader.upload("./public/images/Tom.jpg", function(result) {
 //    console.log(result);
